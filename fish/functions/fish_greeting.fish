3bbf34c3125fc6f923b1ce52a1110d113d46188a
Dan Nixon <dan@dan-nixon.com>
2014-02-03 16:09:37 +0000

    Gave shell multiple personalities

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,3 +1,12 @@
 function fish_greeting
-	fortune ~/dotfiles/fortune/yandere
+  set -l day (date '+%d')
+  set -l type (math $day%3)
+  switch $type
+    case 0
+    	fortune ~/dotfiles/fortune/yandere
+    case 1
+    	fortune ~/dotfiles/fortune/tsundere
+    case 2
+    	fortune ~/dotfiles/fortune/mysterious
+  end
 end
