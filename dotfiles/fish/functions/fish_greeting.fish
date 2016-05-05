706b48b9b5226278968b6fe1cb649a33a48e7993
Dan Nixon <dan@dan-nixon.com>
2016-05-05 13:06:04 +0100

    rm fortune

--- dotfiles/fish/functions/fish_greeting.fish
+++ dotfiles/fish/functions/fish_greeting.fish
@@ -1,12 +1,2 @@
 function fish_greeting
-  set -l day (date '+%d')
-  set -l type (math $day%3)
-  switch $type
-    case 0
-    	fortune ~/dotfiles/fortune/yandere
-    case 1
-    	fortune ~/dotfiles/fortune/tsundere
-    case 2
-    	fortune ~/dotfiles/fortune/mysterious
-  end
 end
