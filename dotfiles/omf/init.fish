ce1f680dd22a95e6fc4893901525b0083ea887fc
Dan Nixon <dan@dan-nixon.com>
2018-03-16 16:01:34 +0000

    Fish init cleanup

--- dotfiles/omf/init.fish
+++ dotfiles/omf/init.fish
@@ -1,16 +1,26 @@
 # Load aliases
 . ~/dotfiles/dotfiles/shell/common_aliases
 
-function fish_add_dir_to_path
-  set --universal fish_user_paths $fish_user_paths $argv
-end
-
-function b
-  upower -i (upower -e | grep 'BAT') | grep -E "state|time\ to|percentage|energy-rate" --color=never
-end
-
 # No greeting
 function fish_greeting
 end
 
+# Append to PATH helper function
+function fish_add_dir_to_path
+  set --universal fish_user_paths $fish_user_paths $argv
+end
+
+# Battery status
+function b
+  upower -i (upower -e | grep 'BAT') | grep -E "state|time\ to|percentage|energy-rate" --color=never
+end
+
+# Direnv
 eval (direnv hook fish)
+
+# Environment Modules
+set modules_path /usr/local/Modules/
+if test -d $modules_path;
+  . $modules_path"init/fish"
+  . $modules_path"init/fish_completion"
+end
