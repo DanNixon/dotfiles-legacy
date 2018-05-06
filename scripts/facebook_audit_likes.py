1eb72b401652f6e7a679b9a514e3ca02671c8125
Dan Nixon <dan@dan-nixon.com>
2018-05-06 18:18:02 +0100

    Update facebook like script

--- scripts/facebook_audit_likes.py
+++ scripts/facebook_audit_likes.py
@@ -2,10 +2,53 @@
 
 # Requires https://github.com/mobolic/facebook-sdk
 
+import sys
+import webbrowser
+
 import click
 import facebook
 
-import webbrowser
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+
+def process_batch(batch):
+    for l in batch:
+        click.echo('> {}'.format(l['name']))
+
+    if query_yes_no('Open in browser?', 'no'):
+        for l in batch:
+            webbrowser.open('https://www.facebook.com/{}'.format(l['id']))
 
 
 @click.command()
@@ -13,13 +56,12 @@ import webbrowser
 def main(token):
     graph = facebook.GraphAPI(access_token=token, version="3.0")
 
-    count = 0
+    batch = []
     for l in graph.get_all_connections(id='me', connection_name='likes'):
-        webbrowser.open('https://www.facebook.com/{}'.format(l['id']))
-        count += 1
-        if count >= 7:
-            input("Press Enter to open next batch...")
-            count = 0
+        batch.append(l)
+        if len(batch) >= 8:
+            process_batch(batch)
+            batch = []
 
 
 if __name__ == '__main__':
