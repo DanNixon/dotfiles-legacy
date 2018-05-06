e737c005503681845444427d2f6e05e425def124
Dan Nixon <dan@dan-nixon.com>
2018-05-06 18:04:24 +0100

    Bookmark helper aliases

--- dotfiles/omf/init.fish
+++ dotfiles/omf/init.fish
@@ -10,6 +10,11 @@ function fish_add_dir_to_path
   set --universal fish_user_paths $fish_user_paths $argv
 end
 
+# Print voile output via less
+function bmless
+  voile $argv | less -r
+end
+
 # Battery status
 function b
   upower -i (upower -e | grep 'BAT') | grep -E "state|time\ to|percentage|energy-rate" --color=never
