30b9470554adc16ca81db30d9678b79b3605fa63
Dan Nixon <dan@dan-nixon.com>
2018-12-08 19:20:46 +0000

    Minor changes

--- dotfiles/polybar/syncthing_status.sh
+++ dotfiles/polybar/syncthing_status.sh
@@ -2,7 +2,7 @@
 
 COLOUR_BAD="cc9090"
 
-if [ "$(pgrep syncthing)" ]; then
+if [ "$(pgrep -x syncthing)" ]; then
   echo "痢"
 else
   echo "裏 %{F#${COLOUR_BAD}}%{u#${COLOUR_BAD}}Syncthing not running%{u-}%{F-}"
