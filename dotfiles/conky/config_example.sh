e8c388d2d97f9f341bfb69367a6cfecbf21a9aa2
Dan Nixon <dan@dan-nixon.com>
2019-02-07 17:08:34 +0000

    Update Conky example

--- dotfiles/conky/config_example.sh
+++ dotfiles/conky/config_example.sh
@@ -8,6 +8,9 @@ cat "sections/separator"
 cat "sections/system"
 cat "sections/separator"
 ./templates/filesystem /
+./templates/drive /dev/sda
+cat "sections/separator"
+./templates/wireless interface
 cat "sections/separator"
 ./templates/process_checker syncthing  Syncthing
 cat "sections/separator"
