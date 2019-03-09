f5a4285779691fd5a62b8112da17eda5ccfea107
Dan Nixon <dan@dan-nixon.com>
2019-03-09 13:03:46 +0000

    Remove write flag on downloaded files

--- scripts/fdroid-download-apps.sh
+++ scripts/fdroid-download-apps.sh
@@ -8,14 +8,15 @@ package_url=`echo "$urls" | jq -r '.package'`
 signature_url=`echo "$urls" | jq -r '.package_signature'`
 
 package_filename=`basename "$package_url"`
+signature_filename=`basename "$signature_url"`
 
 # Download binary and signature if the binary does not exist
 if [ ! -f "$package_url" ]; then
   wget "$package_url"
   wget "$signature_url"
-fi
 
-signature_filename=`basename "$signature_url"`
+  chmod -w "$package_filename" "$signature_filename"
+fi
 
 # Get GPG binary
 GPG=`gpgconf --list-components | grep "OpenPGP" | cut -d: -f3`
