b5b85cdc864db95e69a12c532dd9ec6eb5e3904f
Dan Nixon <dan@dan-nixon.com>
2019-03-24 13:45:36 +0000

    Polybar on multiple monitors

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -6,4 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bottom &
+# Launch bottom bar on all connected monitors
+# See: https://github.com/jaagr/polybar/issues/763#issuecomment-331604987
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar bottom &
+  done
+else
+  polybar bottom &
+fi
