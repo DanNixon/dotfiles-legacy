da9111da98307f47a5508cc69684b1054d587979
Dan Nixon <dan@dan-nixon.com>
2019-05-20 21:58:44 +0100

    General Fish tidy up, remove OMF

---
+++ dotfiles/fish/fish_prompt.fish
@@ -0,0 +1,60 @@
+function _powerline_section_begin
+  set_color --background $argv[1]
+  set_color $fish_color_normal
+  set_color black
+  printf ' '
+end
+
+function _powerline_section_transition
+  printf ' '
+  set_color $argv[1]
+  set_color --background $argv[2]
+  printf ''
+  printf ' '
+  set_color black
+end
+
+function _powerline_section_end
+  set_color $argv[1]
+  printf ' '
+  set_color --background black
+  printf ''
+  set_color black
+end
+
+function fish_prompt
+  set user (whoami)
+  set host (hostname)
+
+  switch $fish_bind_mode
+    case default
+        _powerline_section_begin $fish_color_vi_normal
+        printf '[N]'
+        _powerline_section_transition $fish_color_vi_normal $fish_color_host
+    case insert
+        _powerline_section_begin $fish_color_vi_insert
+        printf '[I]'
+        _powerline_section_transition $fish_color_vi_insert $fish_color_host
+    case replace_one replace-one
+        _powerline_section_begin $fish_color_vi_replace
+        printf '[R]'
+        _powerline_section_transition $fish_color_vi_replace $fish_color_host
+    case visual
+        _powerline_section_begin $fish_color_vi_visual
+        printf '[V]'
+        _powerline_section_transition $fish_color_vi_visual $fish_color_host
+  end
+
+  printf "$host"
+  _powerline_section_transition $fish_color_host $fish_color_cwd
+  printf "$PWD"
+  _powerline_section_end $fish_color_cwd
+
+  printf ' '
+end
+
+function fish_right_prompt
+end
+
+function fish_mode_prompt
+end
--- install/dotfiles/fish.sh
+++ install/dotfiles/fish.sh
@@ -4,11 +4,5 @@ HERE="$( cd "$(dirname "$0")" ; pwd -P )"
 . "$HERE/conf.sh"
 
 df_link \
-  "$DOTFILES/dotfiles/omf" \
-  "$HOME/.config/omf"
-
-set -x
-
-curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install > omf_install && \
-  fish omf_install --noninteractive && \
-  rm omf_install
+  "$DOTFILES/dotfiles/fish" \
+  "$HOME/.config/fish/conf.d"
