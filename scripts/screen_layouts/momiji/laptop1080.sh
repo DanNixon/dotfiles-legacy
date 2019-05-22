b0994ba50a714a7b4689eac45b39f5f748369402
Dan Nixon <dan@dan-nixon.com>
2019-05-22 20:25:43 +0100

    Scripts for 1080p laptop display

---
+++ scripts/screen_layouts/momiji/laptop1080.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+xrandr --output HDMI-1-2 --off --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-1 --off --output DP-1-1 --off --output DP-1-2 --off
+
+cp Xresources_laptop1080 ~/.Xresources
+xrdb ~/.Xresources
+
+polybar-launch
