2a1031a1f4e293002ab7873fa60b012657dc1394
Dan Nixon <dan@dan-nixon.com>
2019-08-13 10:02:42 +0100

    Install Neovim globally

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-mkdir -p "$HOME/.local/bin"
-cd "$HOME/.local/bin"
-
 curl \
   --output nvim \
   --location \
   'https://github.com/neovim/neovim/releases/download/v0.3.8/nvim.appimage'
 
 chmod +x nvim
+
+sudo mkdir -p '/usr/local/bin'
+sudo mv nvim '/usr/local/bin'
