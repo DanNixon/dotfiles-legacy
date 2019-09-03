8d644b3d365ba1f19acf986c6e6c2bfc91ee4586
Dan Nixon <dan@dan-nixon.com>
2019-09-03 15:23:56 +0100

    Improve screen layout scripts

--- scripts/screen_layouts/momiji/screen_layout_desk.sh
+++ scripts/screen_layouts/momiji/screen_layout_desk.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+set -x
+
+HERE="$( cd "$(dirname "$0")" ; pwd -P )"
 
 xrandr --output HDMI-1-2 --off --output eDP-1-1 --off --output HDMI-1-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-1 --off --output DP-1-2 --off
 
-cp Xresources_desk ~/.Xresources
-xrdb ~/.Xresources
+cp "$HERE/Xresources_desk" "$HOME/.Xresources"
+xrdb "$HOME/.Xresources"
 
 polybar-launch
 i3-msg restart
