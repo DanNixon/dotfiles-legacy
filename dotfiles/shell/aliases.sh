066dc61657e5ab46552cbf37d40749f937dc9b9f
Dan Nixon <dan@dan-nixon.com>
2019-09-04 11:31:59 +0100

    Tidy up aliases

--- dotfiles/shell/aliases.sh
+++ dotfiles/shell/aliases.sh
@@ -2,29 +2,47 @@ alias l="ls --color=auto -lFh"
 alias ll="ls --color=auto -alFh"
 alias la="ls --color=auto -A"
 alias ldev="ls --color=auto -lFh /dev/ | grep"
-alias die="sudo shutdown -P now"
+
 alias df="df -h"
 alias du="du -h"
 alias free="free -h"
+
 alias lb="lsblk"
+
 alias q="exit"
 alias c="clear"
-alias g="git"
-alias gs="git s"
+
 alias ..="cd .."
-alias fif="grep -rnw . -e"
+
 alias udm="udisksctl mount -b"
 alias udum="udisksctl unmount -b"
+
+alias xcc="xclip -sel clip"
+alias xcp="xclip -sel clip -o"
+
+alias g="git"
+alias gs="git s"
+
+alias i3-layout-default="i3-msg layout default"
+alias i3-layout-stacking="i3-msg layout stacking"
+alias i3-layout-tabbed="i3-msg layout tabbed"
+
 alias s="screen"
 alias sls="screen -ls"
 alias ss="screen -S"
+
 alias bm="voile"
 alias bmadd="voile add -cae"
+
 alias dk="docker"
 alias dkps="docker ps --format '{{.ID}} ~ {{.Names}} ~ {{.Status}} ~ {{.Image}}'"
 alias dkpsa="docker ps | less -S"
 alias dke="docker exec -it"
 alias dkc="docker-compose"
+
+alias cmake-format-all="find . -type f -name 'CMakeLists.txt' | xargs cmake-format -i"
+alias clang-format-all="find . -type f -regex '.*\.\(c\|cpp\|h\|hpp\)' | xargs clang-format -i"
+
 alias v="nvim"
 alias t="todo.sh"
 alias m="ncmpcpp"
@@ -32,13 +50,7 @@ alias stripexif="exiftool -all="
 alias tok="tomb open -k -"
 alias conda-init="eval $HOME/miniconda3/bin/conda 'shell.zsh' 'hook' $argv | source"
 alias ipy="ipython"
-alias xcc="xclip -sel clip"
-alias xcp="xclip -sel clip -o"
 alias on-file-change="inotifywait -q -e modify"
 alias system-information="sudo lshw | less"
 alias b="upower -i $(upower -e | grep 'BAT')"
-alias i3-layout-default="i3-msg layout default"
-alias i3-layout-stacking="i3-msg layout stacking"
-alias i3-layout-tabbed="i3-msg layout tabbed"
-alias cmake-format-all="find . -type f -name 'CMakeLists.txt' | xargs cmake-format -i"
-alias clang-format-all="find . -type f -regex '.*\.\(c\|cpp\|h\|hpp\)' | xargs clang-format -i"
+alias fif="grep -rnw . -e"
