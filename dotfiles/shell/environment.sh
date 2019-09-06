271787ed2a1a431b7bef31750dfe14ad14f6558a
Dan Nixon <dan@dan-nixon.com>
2019-09-06 08:11:59 +0100

    PATH additions

--- dotfiles/shell/environment.sh
+++ dotfiles/shell/environment.sh
@@ -5,4 +5,4 @@ export TODOTXT_DEFAULT_ACTION='ls'
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 
-export PATH="$HOME/dotfiles/scripts/:$HOME/dotfiles/third_party/scripts/:$HOME/dotfiles/third_party/bin/:$PATH"
+export PATH="$HOME/dotfiles/scripts/:$HOME/.local/bin/:$HOME/dotfiles/third_party/scripts/:$HOME/dotfiles/third_party/bin/:$PATH"
