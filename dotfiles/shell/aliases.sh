e1df3381af94647f15a8ea20e317f9ccda74ab33
Dan Nixon <dan@dan-nixon.com>
2019-09-20 18:45:58 +0100

    ZFS list alias

--- dotfiles/shell/aliases.sh
+++ dotfiles/shell/aliases.sh
@@ -39,7 +39,7 @@ alias dkpsa="docker ps | less -S"
 alias dke="docker exec -it"
 alias dkc="docker-compose"
 
-alias zfs-mounted="zfs list -o name,mounted,encryption,mountpoint"
+alias zfs-ls="zfs list -o name,referenced,used,available,encryption,mounted,mountpoint"
 
 alias cmake-format-all="find . -type f -name 'CMakeLists.txt' | xargs cmake-format -i"
 alias clang-format-all="find . -type f -regex '.*\.\(c\|cpp\|h\|hpp\)' | xargs clang-format -i"
