f4fbf36e7f312955ffe39d99b5d2b671946ee980
Dan Nixon <dan@dan-nixon.com>
2019-11-26 16:46:26 +0000

    Convert DE dotfiles

--- install/dotfiles/i3wm.sh
+++ install/dotfiles/i3wm.sh
@@ -3,25 +3,22 @@
 here="$( cd "$(dirname "$0")" ; pwd -P )"
 . "$here/common.sh"
 
-df_link \
-  "$DOTFILES/dotfiles/dot_config/polybar" \
-  "$HOME/.config/polybar"
+operation="df_$1"
+
+"$operation" \
+  "$DOTFILES/dotfiles/dot_config/i3/config" \
+  "$HOME/.config/i3/config"
+
+"$operation" \
+  "$DOTFILES/dotfiles/dot_config/polybar/config" \
+  "$HOME/.config/polybar/config"
 
 set -x
 
-# base16 theme
-theme="base16-atelier-savanna"
-
-# Make config directory
-i3_config_dir="$HOME/.config/i3"
-mkdir -p "$i3_config_dir"
-
-# Separate config filenames
-theme_config_file="$DOTFILES/dotfiles/i3/base16-i3/colors/${theme}.config"
-common_confg_file="$DOTFILES/dotfiles/i3/config"
-
-# Generate config file
-cat "$theme_config_file" "$common_confg_file" > "$i3_config_dir/config"
+# Copy Polybar scripts
+cp \
+  "$DOTFILES/dotfiles/dot_config/polybar/"*.sh \
+  "$HOME/.config/polybar/"
 
 # Restart i3wm
 i3-msg restart
