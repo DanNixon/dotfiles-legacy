bea38212be4217e454c570ee0ab48299a4dac273
Dan Nixon <dan@dan-nixon.com>
2019-11-27 08:18:52 +0000

    Add Xresources

--- install/dotfiles/i3wm.sh
+++ install/dotfiles/i3wm.sh
@@ -13,12 +13,19 @@ patch_operation="df_$1"
   "$DOTFILES/dotfiles/dot_config/polybar/config" \
   "$HOME/.config/polybar/config"
 
+"$patch_operation" \
+  "$DOTFILES/dotfiles/home/Xresources" \
+  "$HOME/.Xresources"
+
 set -x
 
 # Copy Polybar scripts
 cp \
   "$DOTFILES/dotfiles/dot_config/polybar/"*.sh \
   "$HOME/.config/polybar/"
+
+# Reload xrdb
+xrdb "$HOME/.Xresources"
 
 # Restart i3wm
 i3-msg restart
