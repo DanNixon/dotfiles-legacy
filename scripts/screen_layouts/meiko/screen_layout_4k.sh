b9307b9402d60a00b6158890ac74a5d9add54139
Dan Nixon <dan@dan-nixon.com>
2020-03-26 07:47:20 +0000

    Update screen layouts for work laptop

--- scripts/screen_layouts/meiko/screen_layout_4k.sh
+++ scripts/screen_layouts/meiko/screen_layout_4k.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-xrandr --output eDP-1-1 --mode 3840x2160
+xrandr \
+  --output eDP-1 --primary --mode 3840x2160 \
+  --output HDMI-1 --off
+
 $HOME/dotfiles/install/desktop_environment_config.sh install
 $HOME/.fehbg
--- scripts/screen_layouts/meiko/screen_layout_wfh.sh
+++ scripts/screen_layouts/meiko/screen_layout_wfh.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 xrandr \
-  --output eDP-1 --primary --mode 1920x1080 \
-  --output HDMI-1 --off
+  --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 \
+  --output HDMI-1 --mode 1920x1080 --pos 0x0
 
 PATCH_TARGET="$(hostname)_1080p" $HOME/dotfiles/install/desktop_environment_config.sh install
 $HOME/.fehbg
