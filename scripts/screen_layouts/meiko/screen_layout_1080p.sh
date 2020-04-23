14e3c503c6b4846c4d36c98be818978b4dcf8764
Dan Nixon <dan@dan-nixon.com>
2020-04-23 19:48:19 +0100

    Remove non-functional screen layout config

--- scripts/screen_layouts/meiko/screen_layout_1080p.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-xrandr \
-  --output eDP-1 --primary --mode 1920x1080 \
-  --output HDMI-1 --off
-
-PATCH_TARGET="$(hostname)_1080p" $HOME/dotfiles/install/desktop_environment_config.sh install
