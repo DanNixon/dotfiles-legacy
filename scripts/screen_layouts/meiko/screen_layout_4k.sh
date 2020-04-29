6f0a3ff728e7580f4c32a0ad505c3f4d9b6efd67
Dan Nixon <dan@dan-nixon.com>
2020-04-29 21:26:25 +0100

    Fix DE scripts

--- scripts/screen_layouts/meiko/screen_layout_4k.sh
+++ scripts/screen_layouts/meiko/screen_layout_4k.sh
@@ -4,4 +4,4 @@ xrandr \
   --output eDP-1 --primary --mode 3840x2160 \
   --output HDMI-1 --off
 
-$HOME/dotfiles/install/desktop_environment_config.sh install
+$HOME/dotfiles/install/desktop_environment_xorg.sh install
--- scripts/screen_layouts/meiko/screen_layout_wfh.sh
+++ scripts/screen_layouts/meiko/screen_layout_wfh.sh
@@ -4,4 +4,4 @@ xrandr \
   --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 \
   --output HDMI-1 --mode 1920x1080 --pos 0x0
 
-PATCH_TARGET="$(hostname)_1080p" $HOME/dotfiles/install/desktop_environment_config.sh install
+PATCH_TARGET="$(hostname)_1080p" $HOME/dotfiles/install/desktop_environment_xorg.sh install
