a1bb741a3f14abf6babe73b4e8acbd803eb4d1dd
Dan Nixon <dan@dan-nixon.com>
2020-05-04 07:53:41 +0100

    Remove old configs

--- scripts/screen_layouts/meiko/screen_layout_4k.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-xrandr \
-  --output eDP-1 --primary --mode 3840x2160 \
-  --output HDMI-1 --off
-
-$HOME/dotfiles/install/desktop_environment_xorg.sh install
--- scripts/screen_layouts/meiko/screen_layout_wfh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-xrandr \
-  --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 \
-  --output HDMI-1 --mode 1920x1080 --pos 0x0
-
-PATCH_TARGET="$(hostname)_1080p" $HOME/dotfiles/install/desktop_environment_xorg.sh install
