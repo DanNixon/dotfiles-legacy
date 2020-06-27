6b8eecced3d330f409f48701c2e0817c3504d6e9
Dan Nixon <dan@dan-nixon.com>
2020-06-27 14:46:34 +0100

    Replace forwarded Netdata port with non-default

--- dotfiles/dot_config/shell_common/global_aliases.sh
+++ dotfiles/dot_config/shell_common/global_aliases.sh
@@ -1,3 +1,3 @@
 alias -g fwd-gpg='-R /run/user/1000/gnupg/S.gpg-agent:$(gpgconf --list-dirs agent-extra-socket)'
-alias -g fwd-nd='-L localhost:19999:localhost:19999'
+alias -g fwd-nd='-L localhost:29999:localhost:19999'
 alias -g fwd-st='-L localhost:9999:localhost:8384'
